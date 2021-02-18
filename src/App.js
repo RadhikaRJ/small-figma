ada1866c815ba214515c874397c266d03d8983b3
RadhikaRJ <radhika7002@gmail.com>
2021-02-18 19:06:14 +0530

    basic Font-size implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function SmallFigma(){
+
+
+  return(
+    <div>
+
+      <h1>Sharks can live for five centuries.</h1>
+      <p style={{fontSize:"20px"}}>Greenland Sharks are known to be some of the oldest living animals in our world. Researchers did carbon dating on a Greenland Shark that was caught in 2014 and found it to be around 392 years old. Further testing revealed that our fishy friends could be up to 500 years old. Yes, that would mean that our geriatric friends would have been alive when Leonardo Da Vinci painted the "Mona Lisa."</p>
+    </div>
+  );
+}
 function App() {
+
   return (
     <div className="App">
-      
+      <h1 class="heading">RJ's Themes</h1>
+      <SmallFigma/>
     </div>
   );
 }

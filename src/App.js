44b2a25d7b3938e63d356aadbc5acb991043d14c
RadhikaRJ <radhika7002@gmail.com>
2021-02-19 12:51:30 +0530

    feat: inc/dec text size with buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 
 
 function SmallFigma(){
 
+  const [sizeOfFont,setSizeOfFont]=useState(8);
+  
+  function increaseFontSize(){
+    setSizeOfFont(sizeOfFont+8);
+  }
 
+  function decreaseFontSize(){
+    setSizeOfFont(sizeOfFont-8);
+  }
+
+  function updateParaStyle(){
+    return{
+      fontSize:sizeOfFont,
+    }
+      
+      
+  
+  }
+  
   return(
     <div>
-
+     
       <h1>Sharks can live for five centuries.</h1>
-      <p style={{fontSize:"20px"}}>Greenland Sharks are known to be some of the oldest living animals in our world. Researchers did carbon dating on a Greenland Shark that was caught in 2014 and found it to be around 392 years old. Further testing revealed that our fishy friends could be up to 500 years old. Yes, that would mean that our geriatric friends would have been alive when Leonardo Da Vinci painted the "Mona Lisa."</p>
+      <p style={updateParaStyle()}>Greenland Sharks are known to be some of the oldest living animals in our world. Researchers did carbon dating on a Greenland Shark that was caught in 2014 and found it to be around 392 years old. Further testing revealed that our fishy friends could be up to 500 years old. Yes, that would mean that our geriatric friends would have been alive when Leonardo Da Vinci painted the "Mona Lisa."</p>
+    <p>Style your Text</p>
+    <button 
+    onClick={increaseFontSize}
+    >Increase Size</button>
+
+    <span> px: {sizeOfFont} </span>
+
+    <button
+    onClick={decreaseFontSize}
+    >Decrease Size</button>
     </div>
   );
 }

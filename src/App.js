f32f0d8b72408edd91eba72f07f112681cca9dae
RadhikaRJ <radhika7002@gmail.com>
2021-02-19 13:13:18 +0530

    Style heading size

--- src/App.js
+++ src/App.js
@@ -6,36 +6,46 @@ import { useState } from 'react';
 
 function SmallFigma(){
 
-  const [sizeOfFont,setSizeOfFont]=useState(8);
+  const [textSize,setTextSize]=useState(8);
+  const [HeadingSize,setHeadingSize]=useState(16);
   
   function increaseFontSize(){
-    setSizeOfFont(sizeOfFont+8);
+    setTextSize(textSize+8);
+    setHeadingSize(HeadingSize+8);
   }
 
   function decreaseFontSize(){
-    setSizeOfFont(sizeOfFont-8);
+    setTextSize(textSize-8);
+    setHeadingSize(HeadingSize-8);
   }
 
   function updateParaStyle(){
     return{
-      fontSize:sizeOfFont,
+      fontSize:textSize,
     }
-      
-      
-  
+       }
+
+       function updateHeadingStyle(){
+        return{
+          fontSize:HeadingSize
+        }
   }
   
+
+  
+
+
   return(
     <div>
      
-      <h1>Sharks can live for five centuries.</h1>
+      <h1 style={updateHeadingStyle()}>Sharks can live for five centuries.</h1>
       <p style={updateParaStyle()}>Greenland Sharks are known to be some of the oldest living animals in our world. Researchers did carbon dating on a Greenland Shark that was caught in 2014 and found it to be around 392 years old. Further testing revealed that our fishy friends could be up to 500 years old. Yes, that would mean that our geriatric friends would have been alive when Leonardo Da Vinci painted the "Mona Lisa."</p>
     <p>Style your Text</p>
     <button 
     onClick={increaseFontSize}
     >Increase Size</button>
 
-    <span> px: {sizeOfFont} </span>
+    <span> Text Size px: <strong>{textSize}</strong> Heading Size px: <strong>{HeadingSize}</strong> </span>
 
     <button
     onClick={decreaseFontSize}
